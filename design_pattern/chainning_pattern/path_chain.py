0e5cf2ba24525233952a1db38c021d739ce2b405
Zhao <runzhao@ebay.com>
2019-07-09 12:35:44 +0800

    add chainning_pattern

---
+++ design_pattern/chainning_pattern/path_chain.py
@@ -0,0 +1,30 @@
+class GetPath(object):
+    def __init__(self, path='') -> None:
+        super().__init__()
+        self._path = path
+
+    def __getattr__(self, path):
+        return GetPath('%s/%s' % (self._path, path))
+
+    def __call__(self, path):
+        return GetPath('%s/%s' % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+
+def build_path():
+    # the dot in object(). will invoke the __str__ of the object
+    path01 = GetPath().var.log.nginx
+    print(path01)
+
+
+def append_path():
+    # the parentheses in object() will invoke the __call__ of the object
+    path02 = GetPath()('var')('log')('nginx')
+    print(path02)
+
+
+if __name__ == '__main__':
+    build_path()
+    append_path()
