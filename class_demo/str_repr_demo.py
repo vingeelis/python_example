4e85639290c960cd37db489eded6872a2ac21ed2
Zhao <runzhao@ebay.com>
2019-07-31 13:51:03 +0800

    update django2_demo, class_demo

--- class_demo/str_repr_demo.py
+++ class_demo/str_repr_demo.py
@@ -1,37 +1,36 @@
+#!/usr/bin/env python3
+#
+
+
 class Person(object):
-    name = ""
-    age = 0
+
+    def __init__(self, name="", age=0) -> None:
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return 'Person(name=' + self.name + ', age=' + str(self.age) + ')'
 
 
 class PersonV1(Person):
 
-    def __init__(self, personName, personAge):
-        self.name = personName
-        self.age = personAge
-
-    def __str__(self):
-        return 'Person(name=' + self.name + ', age=' + str(self.age) + ')'
+    def __init__(self, name, age):
+        super().__init__(name, age)
 
     def __repr__(self):
         return {'name': self.name, 'age': self.age}
 
 
 class PersonV2(Person):
-    name = ""
-    age = 0
 
-    def __init__(self, personName, personAge):
-        self.name = personName
-        self.age = personAge
-
-    def __str__(self):
-        return 'Person(name=' + self.name + ', age=' + str(self.age) + ')'
+    def __init__(self, name, age):
+        super().__init__(name, age)
 
     def __repr__(self):
         return '{name:' + self.name + ', age:' + str(self.age) + '}'
 
 
-def printStr(p: PersonV1):
+def print_str(p: PersonV1):
     print("<in printStr>")
     print(p)
     print(p.__str__())
@@ -40,7 +39,7 @@ def printStr(p: PersonV1):
     print()
 
 
-def printReprV1(p: PersonV1):
+def print_repr_v1(p: PersonV1):
     print("<in printReprV1>")
     print(p.__repr__())
     print(type(p.__repr__()))
@@ -51,7 +50,7 @@ def printReprV1(p: PersonV1):
     print()
 
 
-def printReprV2(p: PersonV2):
+def print_repr_v2(p: PersonV2):
     print("<in printReprV2>")
     print(p.__repr__())
     print(type(p.__repr__()))
@@ -61,7 +60,7 @@ def printReprV2(p: PersonV2):
 
 if __name__ == '__main__':
     p1 = PersonV1('Pankaj', 34)
-    printStr(p1)
-    printReprV1(p1)
+    print_str(p1)
+    print_repr_v1(p1)
     p2 = PersonV2('Pankaj', 34)
-    printReprV2(p2)
+    print_repr_v2(p2)
--- django2_demo/mysite/blog/models.py
+++ django2_demo/mysite/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
 # Create your models here.
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -36,5 +38,28 @@ class Post(models.Model):
     class Meta:
         ordering = ('-publish',)
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}.'.format(self.name, self.post)
