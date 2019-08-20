f38c56fd47a4a6c009310dbb2fbfa9562ea00a49
Zhao <runzhao@ebay.com>
2019-08-20 17:38:16 +0800

    update django2_demo

--- django2_demo/mysite/blog/views.py
+++ django2_demo/mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from taggit.models import Tag
+from django.db.models import Count
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
@@ -75,5 +76,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts,
+                   })
