4c3d502cd17175de29a0b533c5bee651ce2a4ccb
Vingee <runzhao@ebay.com>
2019-09-23 16:39:30 +0800

    add builtin_

---
+++ example/builtin_/bool_.py
@@ -0,0 +1,17 @@
+obj = []
+print(obj, 'is', bool(obj))
+
+obj = [0]
+print(obj, 'is', bool(obj))
+
+obj = 0.0
+print(obj, 'is', bool(obj))
+
+obj = None
+print(obj, 'is', bool(obj))
+
+obj = True
+print(obj, 'is', bool(obj))
+
+obj = 'Easy string'
+print(obj, 'is', bool(obj))
