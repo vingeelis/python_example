d24033234d61022baa29c7d1cbf3a7fab789af7b
vingee <vingeelis@google.com>
2019-10-28 17:10:38 +0800

    2019-10-28 	: auto commit by https://github.com/vingeelis/bash_example/blob/master/git_push.sh

--- django2/bookmarks/account/urls.py
+++ django2/bookmarks/account/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
 
     # register
     path('register/', views.register, name='register'),
+    path('edit', views.edit, name='edit',),
 ]
--- scratches_/get_fd.py
+++
@@ -1,8 +0,0 @@
-# query = '''FQDN[@resourceId="%s"].hostName!NodeServer.nodeServer!AssetServer{@faultDomain}'''
-query = '''FQDN[@resourceId="{}"].hostName!NodeServer.nodeServer!AssetServer{{@faultDomain}}'''
-
-
-with open('hosts_to_add') as hosts:
-    # list([print(query % (host.rstrip())) for host in hosts])
-    list([print(query.format(host.rstrip())) for host in hosts])
-
