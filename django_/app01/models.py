7110cbd0bf8175cc3484be0d4384a990c3087fb8
runzhao <runzhao@ebay.com>
2020-11-07 11:40:00 +0800

    2020-11-07 	: auto commit

--- django_/app01/models.py
+++ django_/app01/models.py
@@ -16,12 +16,12 @@ class Host(models.Model):
     business = models.ForeignKey(to='Business', to_field='id', on_delete=models.CASCADE)
 
 
-class Application(models.Model):
-    name = models.CharField(max_length=32)
-    host = models.ManyToManyField('Host')
-    r = models.ManyToManyField(to="Host")
+# class Application(models.Model):
+#     name = models.CharField(max_length=32)
+#     host = models.ManyToManyField('Host')
+#     r = models.ManyToManyField(to="Host")
 
 
-# class HostToApp(models.Model):
-#     hobj = models.ForeignKey(to="Host", to_field="id", on_delete=models.CASCADE)
-#     aobj = models.ForeignKey(to="Application", to_field="id", on_delete=models.CASCADE)
+class HostToApp(models.Model):
+    hobj = models.ForeignKey(to="Host", to_field="id", on_delete=models.CASCADE)
+    aobj = models.ForeignKey(to="Application", to_field="id", on_delete=models.CASCADE)
